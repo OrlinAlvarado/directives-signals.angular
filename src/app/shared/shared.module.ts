04c310cc927a7124539dac62d7a67aa2e4a4ec29
Orlin Alvarado <oalvaradomorales@gmail.com>
2023-10-10 08:07:32 -0600

    Seccion 23 finalizada

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CustomLabelDirective } from './directives/custom-label.directive';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  providers: [],
+  declarations: [
+    CustomLabelDirective
+  ],
+  exports: [
+    CustomLabelDirective
+  ]
+})
+export class SharedModule { }
