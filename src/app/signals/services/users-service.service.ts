7a92a1a643d7a6ea1a361159fc3dc8ee84fb5111
Orlin Alvarado <oalvaradomorales@gmail.com>
2023-10-12 08:08:51 -0600

    Seccion 24 finalizada

---
+++ src/app/signals/services/users-service.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, map, tap } from 'rxjs';
+import { SingleUserResponse, User } from '../interfaces/user-response.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersServiceService {
+  private http =  inject(HttpClient);
+  private baseUrl = 'https://reqres.in/api/users';
+
+  getUserById( id: number): Observable<User> {
+    return this.http.get<SingleUserResponse>(`${this.baseUrl}/${id}`)
+      .pipe(
+        map( response => response.data),
+        tap( console.log )
+      )
+  }
+}
